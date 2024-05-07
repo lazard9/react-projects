0339fe4534457dc6b47cf8c63b9aecadae03c807
Lazar Dacic <lazar.dacic@gmail.com>
2024-05-07 17:32:20 +0200

    chore: Footer rearanged

--- ldprojects/src/components/footer/Footer.jsx
+++ ldprojects/src/components/footer/Footer.jsx
@@ -1,5 +1,10 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import {
+    faFacebook,
+    faInstagram,
+    faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 import Container from "../grid/Container";
 import Row from "../grid/Row";
@@ -15,12 +20,28 @@ const Footer = () => {
                 <Row>
                     <Col sm={6} md={6} lg={3} xl={3}>
                         <Heading headingTag={5} headingLevel={4}>
-                            About Us
+                            Work Showcase
                         </Heading>
                         <ul className="footer__list">
-                            <li className="footer__list-item"><a href="/">Story</a></li>
-                            <li className="footer__list-item"><a href="/usluge">Clients</a></li>
-                            <li className="footer__list-item"><a href="/o-nama">Testimonials</a></li>
+                            <li className="footer__list-item">
+                                <Link to="/projects-one">ProjecstOne</Link>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col sm={6} md={6} lg={3} xl={3}>
+                        <Heading headingTag={5} headingLevel={4}>
+                            About
+                        </Heading>
+                        <ul className="footer__list">
+                            <li className="footer__list-item">
+                                <Link to="/about">Who Am I</Link>
+                            </li>
+                            <li className="footer__list-item">
+                                <Link to="/blog">Blog</Link>
+                            </li>
+                            <li className="footer__list-item">
+                                <Link to="/contact">Contact</Link>
+                            </li>
                         </ul>
                     </Col>
                     <Col sm={6} md={6} lg={3} xl={3}>
@@ -28,21 +49,18 @@ const Footer = () => {
                             Services
                         </Heading>
                         <ul className="footer__list">
-                            <li className="footer__list-item"><a href="/">Marketing</a></li>
-                            <li className="footer__list-item"><a href="/usluge">Consulting</a></li>
-                            <li className="footer__list-item"><a href="/o-nama">Development</a></li>
-                            <li className="footer__list-item"><a href="/o-nama">Design</a></li>
-                        </ul>
-                    </Col>
-                    <Col sm={6} md={6} lg={3} xl={3}>
-                        <Heading headingTag={5} headingLevel={4}>
-                            Contact Us
-                        </Heading>
-                        <ul className="footer__list">
-                            <li className="footer__list-item"><a href="/">United States</a></li>
-                            <li className="footer__list-item"><a href="/usluge">United Kingdom</a></li>
-                            <li className="footer__list-item"><a href="/o-nama">Australia</a></li>
-                            <li className="footer__list-item"><a href="/o-nama">Support</a></li>
+                            <li className="footer__list-item">
+                                <Link to="/">Marketing</Link>
+                            </li>
+                            <li className="footer__list-item">
+                                <Link to="/">Consulting</Link>
+                            </li>
+                            <li className="footer__list-item">
+                                <Link to="/">Development</Link>
+                            </li>
+                            <li className="footer__list-item">
+                                <Link to="/">Design</Link>
+                            </li>
                         </ul>
                     </Col>
                     <Col sm={6} md={6} lg={3} xl={3}>
@@ -50,9 +68,24 @@ const Footer = () => {
                             Social
                         </Heading>
                         <ul className="footer__list">
-                            <li className="footer__list-item"><a href="https://www.facebook.com"><FontAwesomeIcon icon={faFacebook} /> Facebook</a></li>
-                            <li className="footer__list-item"><a href="https://www.instagram.com"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
-                            <li className="footer__list-item"><a href="https://www.linkedin.com"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a></li>
+                            <li className="footer__list-item">
+                                <a href="https://www.facebook.com">
+                                    <FontAwesomeIcon icon={faFacebook} />
+                                    Facebook
+                                </a>
+                            </li>
+                            <li className="footer__list-item">
+                                <a href="https://www.instagram.com">
+                                    <FontAwesomeIcon icon={faInstagram} />
+                                    Instagram
+                                </a>
+                            </li>
+                            <li className="footer__list-item">
+                                <a href="https://www.linkedin.com">
+                                    <FontAwesomeIcon icon={faLinkedin} />
+                                    LinkedIn
+                                </a>
+                            </li>
                         </ul>
                     </Col>
                 </Row>
